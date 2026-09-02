02e945be08c715831725ab738911abecee35f6f7
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-09-02 13:05:06 +0800

    Начало 'Простой контроллер для страницы'

---
+++ students/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from students.models import Group, Student
+# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id' ,'name', 'group']
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display=["id", "name"]
---
+++ students/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Group(models.Model):
+    name = models.TextField("Название")
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+    def __str__(self) -> str:
+        return self.name    
+
+# Create your models here.
+class Student(models.Model):
+    name=models.TextField("ФИО")
+    group = models.ForeignKey("Group", on_delete=models.CASCADE, null = True)
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
