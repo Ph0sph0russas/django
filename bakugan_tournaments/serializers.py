c10a521068b4c4ba1e4fa7668a8f4c60339970c0
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-09-23 13:08:04 +0800

    Начато исправление сериализаторов, чтобы вложенные сериализаторы отображались полностью

--- bakugan_tournaments/serializers.py
+++ bakugan_tournaments/serializers.py
@@ -8,16 +8,20 @@ class TournamentSerializer(serializers.ModelSerializer):
         fields=['id', 'name', 'start_date', 'end_date', 'stages_count', 'prize', 'g_power_limit', 'rules']
 
 class GameSerializer(serializers.ModelSerializer):
+    tournament=TournamentSerializer()
+
     class Meta:
         model = Game
-        fields=['id', 'tournament_id', 'start_date', 'end_date', 'stage', 'status']
+        fields=['id', 'tournament', 'start_date', 'end_date', 'stage', 'status']
 
 class ParticipantsSerializer(serializers.ModelSerializer):
+    game = GameSerializer()
     class Meta:
         model = Participants_in_game
-        fields=['id', 'game_id', 'team_name', 'win_or_lose']
+        fields=['id', 'game', 'team_name', 'win_or_lose']
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    tournament=TournamentSerializer()
     class Meta:
         model = Application
-        fields=['id', 'tournament_id', 'approval']
+        fields=['id', 'tournament', 'approval']
